a9eced5adaba622928c3b6699a711bfd3bb5c5ea
Rayan Mechety <mechety.rayan@gmail.com>
2022-07-17 15:49:13 +0200

    put code in game.js

--- game.js
+++ game.js
@@ -1,24 +1,63 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+function init_player(n) {
+	let sprite = PIXI.Sprite.from("stick.png");
+	sprite.height = HEIGHT / 5;
+	sprite.width = WIDTH / 10;
 
-var game = new Phaser.Game(config);
+	n == 1 ? (sprite.x = WIDTH / 100) : (sprite.x = WIDTH - (WIDTH / 100) * 10);
 
-function preload ()
-{
+	sprite.y = HEIGHT / 2;
+	sprite.movingoffset = 4;
+	sprite.uplimit = 0;
+	sprite.downlimit = HEIGHT - sprite.height;
+	app.stage.addChild(sprite);
+
+	return sprite;
 }
 
-function create ()
-{
+let app = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(app.view);
+const HEIGHT = app.view.height;
+const WIDTH = app.view.width;
+
+app.stage.interactive = true;
+app.stage.on("pointermove", getMousepos);
+
+mousepos = { x: 0, y: 0 };
+function getMousepos(e) {
+	let pos = e.data.global;
+	mousepos.x = pos.x;
+	mousepos.y = pos.y;
+	movePos();
 }
 
-function update ()
-{
+function movePos() {
+	let baseoffset = 5;
+	if (Math.abs(mousepos.y - player.y) < 5) {
+		baseoffset = Math.abs(mousepos.y - player.y);
+	}
+
+	let offset = baseoffset * (mousepos.y > player.y ? 1 : -1);
+
+	if (player.y + offset > player.downlimit || player.y + offset < player.uplimit) return;
+
+	player.y += offset;
 }
+
+let player = init_player(1);
+let player2 = init_player(0);
+
+function update(player) {
+	player.y += player.movingoffset;
+	if (player.y > player.downlimit) {
+		player.movingoffset = -4;
+	} else if (player.y < player.uplimit) {
+		player.movingoffset = 4;
+	}
+}
+setInterval(() => {
+	// update(player);
+	update(player2);
+	// console.log(PIXI.FederatedMouseEvent);
+	// console.log(mousepos.y)
+	if (player.y != mousepos.y) movePos();
+}, 1000 / 60);
